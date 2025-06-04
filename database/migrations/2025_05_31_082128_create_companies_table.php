647bbbf5d5db0f1f4009b274c6be5292196e00de
Rashidli <rashidliseymur@gmail.com>
2025-06-04 17:48:03 +0400

    Test deploy.yml

--- database/migrations/2025_05_31_082128_create_companies_table.php
+++ database/migrations/2025_05_31_082128_create_companies_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('company_id')->constrained()->cascadeOnDelete();
             $table->string('title');
             $table->string('logo')->nullable();
             $table->timestamps();
